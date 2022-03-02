f221e7130437e8cf3f943c1677d72df729913b84
elmar <elmarcera@gmail.com>
2022-03-02 15:23:13 +0800

    Create additional Authority

--- src/main/java/com/hrisgsh/app/security/AuthoritiesConstants.java
+++ src/main/java/com/hrisgsh/app/security/AuthoritiesConstants.java
@@ -9,6 +9,10 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String MANAGER = "ROLE_USER";
+
+    public static final String HR = "ROLE_USER";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
