31701415b178fd6d4f377c25fca512e37853631d
elmar <elmarcera@gmail.com>
2022-03-02 15:27:22 +0800

    Mofify Role

--- src/main/java/com/hrisgsh/app/security/AuthoritiesConstants.java
+++ src/main/java/com/hrisgsh/app/security/AuthoritiesConstants.java
@@ -9,9 +9,9 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
-    public static final String MANAGER = "ROLE_USER";
+    public static final String MANAGER = "ROLE_MANAGER";
 
-    public static final String HR = "ROLE_USER";
+    public static final String HR = "ROLE_HR";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
